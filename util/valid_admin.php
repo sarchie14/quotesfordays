fa1b42b3c01a64d3fb8bed4bdd2e90b601dd992e
sarchie14 <spencer.archibald05@gmail.com>
2020-05-04 17:52:20 -0500

    Update valid_admin.php

--- util/valid_admin.php
+++ util/valid_admin.php
@@ -1,6 +1,6 @@
 <?php 
     //make sure the user is logged in as a valid admin
     if (!isset($_SESSION['is_valid_admin'])) {
-        header("Location: quotes-register.php");
+        header("Location: admin-login.php");
     }
 ?>
